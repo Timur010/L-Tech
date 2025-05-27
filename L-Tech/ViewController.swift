f05a0322b9c04af23f30a34236fbbf60da7346f6
Timur010 <duakar010@gmail.com>
2025-05-27 19:21:55 +0300

    Initial Commit

---
+++ L-Tech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L-Tech
+//
+//  Created by Timur Kadiev on 27.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ L-TechTests/L_TechTests.swift
@@ -0,0 +1,17 @@
+//
+//  L_TechTests.swift
+//  L-TechTests
+//
+//  Created by Timur Kadiev on 27.05.2025.
+//
+
+import Testing
+@testable import L_Tech
+
+struct L_TechTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
